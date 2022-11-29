dd3410d277dcd75b43ed8c82cf782da20593efbb
diegorufe <diegorufe@gmail.com>
2022-11-29 07:34:54 +0100

    Remove taxes

--- backend/java/spring/rf-s-accounting/src/main/java/com/rfindustries/accounting/dao/TaxDao.java
+++
@@ -1,9 +0,0 @@
-package com.rfindustries.accounting.dao;
-
-import com.rfindustries.accounting.entities.TaxEntity;
-import com.rfindustries.corejdbc.dao.BaseJDBCCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaxDao extends BaseJDBCCrudRepository<TaxEntity> {
-}
