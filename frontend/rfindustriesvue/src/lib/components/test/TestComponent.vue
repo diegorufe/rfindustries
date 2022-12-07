951a69658ab0ab5cb2f78313cfb047859af40e08
diegorufe <diegorufe@gmail.com>
2022-12-07 07:37:42 +0100

    Start library components vue

---
+++ frontend/rfindustriesvue/src/lib/components/test/TestComponent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps({
+  msg: {
+    default: "Hello",
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <p>{{ props.msg }}</p>
+</template>
+
+<style scoped></style>
