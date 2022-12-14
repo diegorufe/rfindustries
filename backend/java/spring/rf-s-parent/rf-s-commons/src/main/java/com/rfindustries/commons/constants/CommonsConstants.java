03d14066c56e7ed237d8c3b11422e0e538f215c2
diegorufe <diegorufe@gmail.com>
2022-12-14 06:58:46 +0100

    Start commons module

---
+++ backend/java/spring/rf-s-parent/rf-s-commons/src/main/java/com/rfindustries/commons/constants/CommonsConstants.java
@@ -0,0 +1,10 @@
+package com.rfindustries.commons.constants;
+
+public final class CommonsConstants {
+
+    public static final String ROUTE = "/commons";
+
+    private CommonsConstants(){
+        // NOT IMPLEMENTED
+    }
+}
