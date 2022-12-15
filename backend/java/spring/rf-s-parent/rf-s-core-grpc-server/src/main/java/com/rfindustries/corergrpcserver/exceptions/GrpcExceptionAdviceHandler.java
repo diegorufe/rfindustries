295ee198bb86463206a86809d074885f597a5f76
diegorufe <diegorufe@gmail.com>
2022-12-15 06:02:42 +0100

    Added exception handler grpc and build

---
+++ backend/java/spring/rf-s-parent/rf-s-core-grpc-server/src/main/java/com/rfindustries/corergrpcserver/exceptions/GrpcExceptionAdviceHandler.java
@@ -0,0 +1,34 @@
+package com.rfindustries.corergrpcserver.exceptions;
+
+import com.google.rpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.protobuf.StatusProto;
+import net.devh.boot.grpc.server.advice.GrpcAdvice;
+import net.devh.boot.grpc.server.advice.GrpcExceptionHandler;
+
+/**
+ * <a href="https://github.com/senoritadeveloper01/nils-spring-microservices-grpc/blob/master/city-score/src/main/java/com/nils/microservices/cityscore/exception/CityScoreExceptionHandler.java">...</a>
+ */
+@GrpcAdvice
+public class GrpcExceptionAdviceHandler {
+
+
+    @GrpcExceptionHandler(Throwable.class)
+    public StatusRuntimeException handler() {
+
+
+//        Status status = Status.newBuilder()
+//                .setCode(500)
+//                .setMessage("Error ")
+//                .addDetails(Any.pack(exceptionResponse))
+//                .build();
+
+        Status status = Status.newBuilder()
+                .setCode(500)
+                .setMessage("Error")
+                .build();
+
+        return StatusProto.toStatusRuntimeException(status);
+
+    }
+}
